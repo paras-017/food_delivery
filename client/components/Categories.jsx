cc0549951ba4ee43b2d0b1e0595a5becb5587c35
paras-017 <paraschauhan0017@gmail.com>
2023-08-17 11:27:26 +0530

    categories added

--- client/components/Categories.jsx
+++ client/components/Categories.jsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, FlatList, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { categories } from '../constants'
+
 
 const Categories = () => {
+    const [activeCategory, setActiveCategory] = useState(null);
   return (
     <View>
-      <Text className='text-blue-700'>Categories</Text>
+      <ScrollView  showsHorizontalScrollIndicator={false} className='overflow-visible' contentContainerStyle={{paddingHorizontal:15}}>
+        <FlatList  horizontal={true}  data={categories} renderItem={({item})=>{
+            let isActive = item.id==activeCategory
+            let btnClass = isActive?'bg-gray-600':'bg-gray-200'
+            let textClass = isActive?'font-semibold text-gray-800':'text-gray-500'
+            console.log(item)
+         return (
+            <View key={item.id} className='flex justify-center items-center mr-6'>
+                <TouchableOpacity onPress={()=> setActiveCategory(item.id)}  className={`p-1 rounded-full shadow bg-gray-400 ${btnClass}`}>
+                  <Image style={{width: 45, height: 45}} source={item.image} />
+                </TouchableOpacity>
+                  <Text className={`text-sm text-black ${textClass}`}>{item.name}</Text>
+            </View>
+         )
+        }}/>
+      </ScrollView>
     </View>
   )
 }
